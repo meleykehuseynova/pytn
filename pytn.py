f160b38a78efee996fcd94d90e0fa56597b75e20
meleykehuseynova <161501438+meleykehuseynova@users.noreply.github.com>
2024-05-02 09:16:46 -0700

    Python 3

---
+++ pytn.py
@@ -0,0 +1,42 @@
+# EV TASKI : SUAL1
+
+# newList = []
+# for i in range(5):
+#     list = int(input("reqemi daxil edin: "))
+#     newList.append(list)
+# newList.sort()
+# print( newList)
+
+# ******************************************************************
+# SUAL2
+
+# cumle = input("cumleni daxil edin:")
+# sozler = cumle.split()
+# duz_cumle = ""
+# for soz in sozler:
+#     duz_soz = ''.join(sorted(soz))
+#     duz_cumle += duz_soz + " "
+# print( duz_cumle)
+
+# ***********************************************************************
+# SUAL3
+# n = 13
+# count = 0
+# while True:
+#     a = int(input("Ededi daxil edin: "))
+#     count += 1
+#     if a == n:
+#         print(f"Tebrikler, {count} cehdde {n} ededini tapdnz")
+#         break
+
+# ***********************************************************************
+# SUAL4
+# for eded in range(2, 100):
+#     a = True
+#     for bolen in range(2, int(eded / 2) + 1):
+#         if eded % bolen == 0:
+#             a = False
+#             break
+#     if a:
+#         print(eded)
+
